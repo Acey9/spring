78b794b56bf9c5edee1035354b86d25c3e417ff7
Acey9 <huiwang.e@gmail.com>
2017-04-16 11:28:14 +0800

    add gencert script

---
+++ src/cnc/create_kp.sh
@@ -0,0 +1,20 @@
+read -p "Enter your domain [www.example.com]: " DOMAIN
+
+echo "Create server key..."
+
+openssl genrsa -des3 -out $DOMAIN.key 1024
+
+echo "Create server certificate signing request..."
+
+SUBJECT="/C=CN/ST=SP/L=spring/O=spring/OU=spring/CN=$DOMAIN"
+
+openssl req -new -subj $SUBJECT -key $DOMAIN.key -out $DOMAIN.csr
+
+echo "Remove password..."
+
+mv $DOMAIN.key $DOMAIN.origin.key
+openssl rsa -in $DOMAIN.origin.key -out $DOMAIN.key
+
+echo "Sign SSL certificate..."
+
+openssl x509 -req -days 3650 -in $DOMAIN.csr -signkey $DOMAIN.key -out $DOMAIN.crt
